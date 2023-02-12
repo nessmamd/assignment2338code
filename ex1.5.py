c47c6ad6783ae3c1c2c4a7825c4b25c2ea299bd5
nessmamd <113803210+nessmamd@users.noreply.github.com>
2023-02-12 04:58:07 -0700

    Update ex1.5.py

--- ex1.5.py
+++ ex1.5.py
@@ -1,4 +1,5 @@
 import timeit
+import time 
 import matplotlib.pyplot as plt
 dict_fun = {}
 
@@ -8,37 +9,40 @@ def func(n):
     else:
         return func(n-1) + func(n-2)
 
-def funcnew(n):
-    if n == 0 or n == 1:
-        return n 
+def funcnew(nz):
+    if nz == 0 or nz == 1:
+        return nz 
     else:
-        if(n in dict_fun):
-            return dict_fun[n]
+        if(nz in dict_fun):
+            return dict_fun[nz]
         else:
-            val = funcnew(n-1) + funcnew(n-2)
-            dict_fun[n] = val
-            return val
+            val = funcnew(nz-1) + funcnew(nz-2)
+            dict_fun[nz] = val
+            return val 
 
-c = range(0,36)
-elasped_time = 0 
-elasped_time_new = 0
-y_input = []
+y_input = list(range(1,37))
+y_input_two = list(range(1,37))
 x_input_new = []
 x_input_old = []
 
-for x in c: 
-    y_input.append(x)
-    m = timeit.timeit(lambda : func(x))
-    x_input_old.append(m)
+for x in y_input: 
+    start = time.time()
+    func(x)
+    end = time.time()
+    x_input_old.append(end-start)
 
-for mo in c: 
-    p = timeit.timeit(lambda : funcnew(x))
+
+for mo in y_input_two: 
+    p = timeit.timeit(lambda : funcnew(mo))
     dict_fun.clear()
-    x_input_new.append(p)
+    x_input_new.append(p) 
 
-plt.plot(x_input_new, y_input, label = "modified function") 
-plt.plot(x_input_old, y_input, label = "original function")
-plt.xlabel("Time taken")
-plt.ylabel("value of n ")
+print(x_input_new)
+plt.plot(y_input, x_input_new, label = "modified function") 
+plt.plot(y_input, x_input_old, label = "original function") 
+plt.xlabel("value of n")
+plt.ylabel("time taken ")
 plt.legend()
 plt.show()
+
+    
